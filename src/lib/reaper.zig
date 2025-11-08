f09e2d1c5b6e55402de6ec3140a2a7444be08941
GoranKovac <gorankovacstudio@gmail.com>
2025-11-08 18:48:30 +0100

    zig 0.15 update
    0.15 has no namespaces

--- src/lib/reaper.zig
+++ src/lib/reaper.zig
@@ -1,8 +1,9 @@
 const std = @import("std");
 const main_module = @import("../main.zig");
 const control_surface = @import("../control_surface.zig");
-pub usingnamespace @import("reaper_functions.zig");
+pub const reaper = @import("reaper_functions.zig");
 pub const ImGui = @import("reaper_imgui.zig");
+pub const API = reaper.fnPtrs;
 
 const Self = @This();
 
@@ -10,11 +11,11 @@ const Self = @This();
 var is_looping: bool = false;
 var imgui_init: bool = false;
 var myCsurf: control_surface.c.C_ControlSurface = undefined;
+pub const loop = &extension.loop;
 
 /// Extension bindings structs
 pub const extension = struct {
     // add all reaper function pointers here
-    pub usingnamespace Self.fnPtrs;
 
     // Action_id for registrating extension
     pub var action_id: c_int = undefined;
@@ -27,8 +28,8 @@ pub const extension = struct {
     pub const plugin_info_t = extern struct {
         caller_version: c_int,
         hwnd_main: HWND,
-        register: ?@TypeOf(extension.plugin_register),
-        getFunc: ?@TypeOf(extension.plugin_getapi),
+        register: ?@TypeOf(API.plugin_register),
+        getFunc: ?@TypeOf(API.plugin_getapi),
     };
 
     pub const custom_action_register_t = extern struct {
@@ -46,17 +47,17 @@ pub const extension = struct {
         }
 
         const getFunc = rec.getFunc.?;
-        inline for (@typeInfo(@This()).Struct.decls) |decl| {
-            comptime var decl_type = @typeInfo(@TypeOf(@field(@This(), decl.name)));
-            const is_optional = decl_type == .Optional;
+        inline for (@typeInfo(API).@"struct".decls) |decl| {
+            comptime var decl_type = @typeInfo(@TypeOf(@field(API, decl.name)));
+            const is_optional = decl_type == .optional;
             if (is_optional)
-                decl_type = @typeInfo(decl_type.Optional.child);
-            if (decl_type != .Pointer or @typeInfo(decl_type.Pointer.child) != .Fn)
+                decl_type = @typeInfo(decl_type.optional.child);
+            if (decl_type != .pointer or @typeInfo(decl_type.pointer.child) != .@"fn")
                 continue;
             if (getFunc(decl.name)) |func|
-                @field(@This(), decl.name) = @ptrCast(func)
+                @field(API, decl.name) = @ptrCast(func)
             else if (is_optional)
-                @field(@This(), decl.name) = null
+                @field(API, decl.name) = null
             else {
                 std.debug.print("unable to import the API function '{s}'\n", .{decl.name});
                 return false;
@@ -65,15 +66,16 @@ pub const extension = struct {
 
         return true;
     }
+
     // Needed to import namespaces
     const init = initFunctions;
 
     /// Initialize loop (timer) - Start/Stop
     pub fn loop(set_loop: bool) !void {
         if (set_loop) {
-            _ = extension.plugin_register("timer", @constCast(@ptrCast(&onTimer)));
+            _ = API.plugin_register("timer", @ptrCast(@constCast(&onTimer)));
         } else {
-            _ = extension.plugin_register("-timer", @constCast(@ptrCast(&onTimer)));
+            _ = API.plugin_register("-timer", @ptrCast(@constCast(&onTimer)));
             try main_module.initVars(false);
             std.debug.print("GLOBAL VARS DEINIT\n", .{});
             registerCsurf(false);
@@ -83,31 +85,34 @@ pub const extension = struct {
 };
 
 fn registerCsurf(init: bool) void {
-    if (init) {
-        std.debug.print("CSURF INIT\n", .{});
-        myCsurf = control_surface.init();
-        _ = extension.plugin_register("csurf_inst", myCsurf.?);
-    } else {
-        std.debug.print("CSURF DEINIT\n", .{});
-        _ = extension.plugin_register("-csurf_inst", myCsurf.?);
-        control_surface.deinit(myCsurf);
+    if (main_module.ExtensionCfg.is_csurf == true) {
+        if (init) {
+            std.debug.print("CSURF INIT\n", .{});
+            myCsurf = control_surface.init();
+            _ = API.plugin_register("csurf_inst", myCsurf.?);
+        } else {
+            std.debug.print("CSURF DEINIT\n", .{});
+            _ = API.plugin_register("-csurf_inst", myCsurf.?);
+            control_surface.deinit(myCsurf);
+        }
     }
 }
 
 /// Run when extension is activated via action list or onCommand api call from Reaper
-fn onCommand(sec: *extension.KbdSectionInfo, command: c_int, val: c_int, val2hw: c_int, relmode: c_int, hwnd: extension.HWND) callconv(.C) c_char {
+fn onCommand(sec: *extension.KbdSectionInfo, command: c_int, val: c_int, val2hw: c_int, relmode: c_int, hwnd: extension.HWND) callconv(.c) c_char {
     _ = .{ sec, val, val2hw, relmode, hwnd };
 
     if (command == extension.action_id) {
         // init imgui
-        if (imgui_init == false) {
-            ImGui.init(extension.plugin_getapi) catch {
+        if (main_module.ExtensionCfg.is_imgui == true and imgui_init == false) {
+            ImGui.init(API.plugin_getapi) catch {
                 std.debug.print("IMGUI INIT FAILED\n", .{});
                 return 0;
             };
             std.debug.print("IMGUI INIT\n", .{});
             imgui_init = true;
         }
+
         // init csurf, global variables
         if (is_looping == false) {
             registerCsurf(true);
@@ -117,17 +122,20 @@ fn onCommand(sec: *extension.KbdSectionInfo, command: c_int, val: c_int, val2hw:
                 return 0;
             };
         }
+
         extension.loop(!is_looping) catch {
             std.debug.print("GLOBAL VARS DEINIT FAILED\n", .{});
             return 0;
         };
+
         return 1;
     }
+
     return 0;
 }
 
 /// Call function on timer interval (Reaper 33hz)
-fn onTimer() callconv(.C) void {
+fn onTimer() callconv(.c) void {
     main_module.main() catch {
         // stop on error
         extension.loop(false) catch {
@@ -149,9 +157,10 @@ export fn ReaperPluginEntry(instance: extension.HINSTANCE, rec: ?*extension.plug
     }
     // register extension in action list on reaper startup
     const action = extension.custom_action_register_t{ .section = 0, .id_str = main_module.ExtensionCfg.id, .name = main_module.ExtensionCfg.name };
-    extension.action_id = extension.plugin_register("custom_action", @constCast(@ptrCast(&action)));
+    extension.action_id = API.plugin_register("custom_action", @ptrCast(@constCast(&action)));
     // Run when extension is activated via action list or onCommand api call from Reaper
-    _ = extension.plugin_register("hookcommand2", @constCast(@ptrCast(&onCommand)));
+    _ = API.plugin_register("hookcommand2", @ptrCast(@constCast(&onCommand)));
     std.debug.print("EXTENSION INIT\n", .{});
     return 1;
 }
+
