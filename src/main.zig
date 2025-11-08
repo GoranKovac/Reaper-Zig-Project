4a6261958b4ba3499f218c2a62d5fb7fa7753111
GoranKovac <gorankovacstudio@gmail.com>
2025-11-08 18:50:08 +0100

    better main
    little cleaning up main

--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,18 @@
 // zig build-lib -dynamic -O ReleaseFast -femit-bin=reaper_zig.so
 const std = @import("std");
-const reaper = @import("lib/reaper.zig");
-const ImGui = reaper.ImGui;
+const extension = @import("lib/reaper.zig");
+const r = extension.reaper;
+const im = extension.ImGui;
 // preload reaper.zig so it can register ReaperEntryPoint and call main() from there
 comptime {
-    _ = reaper;
+    _ = extension;
 }
 
 pub const ExtensionCfg = struct {
     pub const id: [*:0]const u8 = "ZIGGY_12345";
     pub const name: [*:0]const u8 = "Zig TEST";
+    pub const is_imgui: bool = true;
+    pub const is_csurf: bool = true;
 };
 
 pub const globalVars = @import("imgui_vars_test.zig");
@@ -24,28 +27,27 @@ pub fn initVars(init: bool) !void {
         globals = null;
     }
 }
-
 // called from extension entrypoint
 pub fn main() !void {
     var g = &globals.?;
 
-    try ImGui.SetNextWindowSize(.{ g.ctx, 400, 80, ImGui.Cond_FirstUseEver });
-
+    try im.SetNextWindowSize(.{ g.ctx, 400, 80, im.Cond_FirstUseEver });
     var open: bool = true;
-    if (try ImGui.Begin(.{ g.ctx, g.plugin_name, &open })) {
-        if (try ImGui.Button(.{ g.ctx, "Click me!" })) {
+
+    if (try im.Begin(.{ g.ctx, g.plugin_name, &open })) {
+        if (try im.Button(.{ g.ctx, "Click me!" })) {
             g.click_count +%= 1;
         }
         var buf: [100]u8 = undefined;
         const res = try std.fmt.bufPrintZ(&buf, "Clicked Ammount {d}", .{g.click_count});
 
-        try ImGui.SameLine(.{g.ctx});
-        try ImGui.Text(.{ g.ctx, res });
-        _ = try ImGui.InputText(.{ g.ctx, "text input", &g.text, g.text.len });
-        try ImGui.End(.{g.ctx});
+        try im.SameLine(.{g.ctx});
+        try im.Text(.{ g.ctx, res });
+        _ = try im.InputText(.{ g.ctx, "text input", &g.text, g.text.len });
+        try im.End(.{g.ctx});
     }
 
     if (!open) {
-        try reaper.extension.loop(false);
+        try extension.loop(false);
     }
 }
